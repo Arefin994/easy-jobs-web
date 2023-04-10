16df3eee6554c0049659ce1e7c7f99928f2f49da
Arefin994 <arefinamin994@gmail.com>
2023-04-10 15:55:26 +0600

    Job Category section Done!!

--- src/components/JobCats/JobCats.jsx
+++ src/components/JobCats/JobCats.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 const JobCats = () => {
-  const [jobCats, setJobCats] = useState([]);
+    const [jobCats, setJobCats] = useState([]);
 
-  useEffect(() => {
-    fetch('/public/job-cats.json')
-      .then(response => response.json())
-      .then(data => setJobCats(data));
-  }, []);
+    useEffect(() => {
+        fetch('/public/job-cats.json')
+            .then(response => response.json())
+            .then(data => setJobCats(data.categories));
+    }, []);
 
-  return (
-    <div className="d-flex">
-      
-    </div>
-  );
+    return (
+        <div className='text-center my-5'>
+            <h2>Job Category List</h2>
+            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className="d-md-flex p-3">
+
+                {jobCats.map((cat, index) => (
+                    <div className="me-4 bg-secondary p-4 rounded-4 my-2" key={index}>
+                        <img src={cat["img-link"]} alt={cat.name} />
+                        <h4>{cat.name}</h4>
+                        <p>{cat.jobsAvailable} Jobs Available</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default JobCats;
